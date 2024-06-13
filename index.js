d0f03d9eb907b5d12579b7d6dfaaacd0a5256df0
Hanah Trexie <74289318+httcg@users.noreply.github.com>
2024-06-13 08:29:51 +0800

    initial form redirection

---
+++ index.js
@@ -0,0 +1,4 @@
+function redirectToNextPage() { 
+    document.getElementById("myForm").submit(); // Submit the form 
+    window.location.href = "educationbg.html"; // Redirect to the next page 
+}
