e6708981f2877b01ae9ca8b3ffb16bd746f7dae1
httcg <74289318+httcg@users.noreply.github.com>
2024-06-27 14:48:49 +0800

    updated

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 function redirectToNextPage() { 
-    document.getElementById("myForm").submit(); // Submit the form 
-    window.location.href = "educationbg.html"; // Redirect to the next page 
+    document.getElementById("myForm").submit(); 
+    window.location.href = "educational-bg.html"; 
+}
+
+function redirectToHealthPage() { 
+    document.getElementById("myForm").submit(); 
+    window.location.href = "health.html"; 
+}
+
+function redirectToFamilyPage() { 
+    document.getElementById("myForm").submit(); 
+    window.location.href = "family-bg.html"; 
 }
