b79d336f62f2b225b889e07af71bce25933230b4
httcg <74289318+httcg@users.noreply.github.com>
2024-07-14 04:12:34 +0800

    ud

--- firebase db test/js files/edu.js
+++ firebase db test/js files/edu.js
@@ -32,3 +32,24 @@ submitButton2.addEventListener("click", function(event) {
    window.location.href = "family.html"; 
 
 });
+
+
+function addEducationRow() {
+    const educationRows = document.getElementById("educationRows");
+    const newRow = educationRows.firstElementChild.cloneNode(true);
+  
+    newRow.querySelectorAll("input[type='text'], input[type='radio']").forEach(input => input.value = "");
+  
+    // add a delete button to the new row
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.textContent = "Delete";
+    newRow.append(deleteButton);
+  
+    deleteButton.addEventListener("click", function() {
+      this.parentElement.remove();
+    });
+  
+    educationRows.appendChild(newRow);
+  }
+  
---
+++ firebase db test/js files/index.js
@@ -0,0 +1,3 @@
+document.getElementById('answer-form').addEventListener('click', function() {
+    window.location.href = 'personal.html';
+});  
